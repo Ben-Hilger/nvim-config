e28a1ffbf09ca8a5a83edb67dd4f1a24ec6c819a
Ben Hilger <bhilger78@gmail.com>
2023-12-27 13:41:40 -0500

    initial config commit

---
+++ lua/config/packer.lua
@@ -0,0 +1,45 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'Mofiqul/vscode.nvim'
+    use {
+      'neovim/nvim-lspconfig',
+	  requires = {
+		  -- LSP Support
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
+  }
+
+  use ('tpope/vim-fugitive')
+  use ('mbbill/undotree')
+
+  use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
+
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+
+end)
