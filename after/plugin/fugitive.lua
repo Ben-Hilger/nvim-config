c55cfd39e1cfdaa1ffbe018f96c4868c8b490aa8
Ben Hilger <bhilger78@gmail.com>
2023-12-28 20:51:33 -0500

    Test modifying git fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>git", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gitpl", function() vim.cmd("Git pull") end);
+vim.keymap.set("n", "<leader>gitph", function() vim.cmd("Git push") end);
